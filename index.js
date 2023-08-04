123acbf76b372c523d6fd610f40fab4678f27681
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-04 16:26:50 +0200

    Lab solved

--- index.js
+++ index.js
@@ -1,23 +1,75 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI =
+  "mongodb+srv://ArvidErik:Arvider@cluster0.8oaqccm.mongodb.net/MyFirstDatabase";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
+  })
+
+  // INSERT ONE RECIPE
+  .then(async () => {
+    const pancake = {
+      title: "Pancake",
+      level: "Easy Peasy",
+      ingredients: ["salt", "water", "flower", "egg"],
+      cuisine: "Hungarian",
+      dishType: "dessert",
+      image: "https://www.mindmegette.hu/images/181/O/palacsinta_csokikrem.jpg",
+      duration: 30,
+      creator: "Erik",
+      created: "",
+    };
+    await Recipe.create(pancake);
+    console.log(pancake.title);
+  })
+  .catch((err) => console.log("error with the pancake", err))
+
+
+  // INSERT MANY RECEPIES
+  .then(async () => {
+    await Recipe.insertMany(data);
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log("Following recepies have been added successfully:");
+    data.forEach((element) => {
+      console.log(element.title);
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((err) => console.log("error with creation many", err))
+
+
+  // UPDATE RIGATONI
+  .then(async () => {
+    const query = { title: "Rigatoni alla Genovese" };
+    await Recipe.findOneAndUpdate(query, { duration: 100 });
+  })
+  .then(() => {
+    console.log("Duration has been updated successfully.");
+  })
+  .catch((err) => console.log(err))
+
+
+  //DELETE CARROT CAKE
+  .then(async () => {
+    await Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then(() => {
+    console.log("Carrotcake has been deleted");
+  })
+  .catch((err) => console.log(err))
+
+
+  //CONNECTION ERROR
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
